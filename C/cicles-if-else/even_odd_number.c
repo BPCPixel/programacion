df3eef32bcea24505efe03adc3caa25e29ce2051
Lalo Tellez <bpcpixel@gmail.com>
2025-12-18 19:47:05 -0600

    Initialize Programacion repo excluding Python project

---
+++ C/cicles-if-else/even_odd_number.c
@@ -0,0 +1,19 @@
+// This program shows if the number is even or odd
+#include <stdio.h>
+
+int main(){
+
+int number;
+
+printf("Type a number: ");
+scanf("%d",&number);
+
+if(number % 2 == 0){
+  printf("This number is even \n");
+}else{
+  printf("This number is odd \n");
+}
+
+
+  return 0;
+}
